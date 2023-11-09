4ef9682bf68d6b01ddc2a4154d1914d24e62f7fb
Carl Thomé <carlthome@gmail.com>
2023-11-09 02:33:52 +0100

    Add mirdata (#4)
    * Add mirdata (and its dependencies)
    
    * Get source from GitHub instead
    
    * Use buildPythonPackage
    
    * Use pytest-xdist and add smart_open to runtime

---
+++ pkgs/development/python-modules/mirdata/default.nix
@@ -0,0 +1,111 @@
+{ lib
+, python3
+, fetchFromGitHub
+, jams
+, pretty-midi
+, music21
+, dali-dataset
+}:
+
+python3.pkgs.buildPythonPackage rec {
+  pname = "mirdata";
+  version = "0.3.8";
+  format = "setuptools";
+
+  # The tests require data files that are not included in the source release, so we take the source from GitHub instead.
+  src = fetchFromGitHub {
+    owner = "mir-dataset-loaders";
+    repo = pname;
+    rev = version;
+    hash = "sha256-Ar0FsMb5S9lvzBLUHf13d8SXWPSSkQRxnUnSzPiu/MU=";
+  };
+
+  propagatedBuildInputs = with python3.pkgs; [
+    attrs
+    black
+    chardet
+    deprecated
+    h5py
+    jams
+    librosa
+    numpy
+    pandas
+    pretty-midi
+    pyyaml
+    requests
+    scipy
+    tqdm
+    dali-dataset
+    smart-open
+  ];
+
+  passthru.optional-dependencies = with python3.pkgs; {
+    cipi = [
+      music21
+    ];
+    compmusic_carnatic_rhythm = [
+      openpyxl
+    ];
+    compmusic_hindustani_rhythm = [
+      openpyxl
+    ];
+    dali = [
+      dali-dataset
+    ];
+    docs = [
+      numpydoc
+      recommonmark
+      sphinx
+      sphinx-rtd-theme
+      sphinx-togglebutton
+      #sphinxcontrib-napoleon
+    ];
+    gcs = [
+      smart-open
+      google-cloud-storage
+    ];
+    haydn_op20 = [
+      music21
+    ];
+    http = [
+      smart-open
+    ];
+    s3 = [
+      smart-open
+    ];
+    tests = [
+      coveralls
+      decorator
+      future
+      pytest
+      pytest-cov
+      pytest-localserver
+      pytest-mock
+      pytest-xdist
+      #pytest-pep8
+      smart-open
+      testcontainers
+      #types-chardet
+      types-pyyaml
+    ];
+  };
+
+  checkInputs = lib.flatten (builtins.attrValues passthru.optional-dependencies);
+
+  checkPhase = ''
+    runHook preCheck
+    export DEFAULT_DATA_HOME=$TEMP
+    export NUMBA_CACHE_DIR=$TEMP
+    ${python3.pkgs.pytest}/bin/pytest -n auto tests/
+    runHook postCheck
+  '';
+
+  pythonImportsCheck = [ "mirdata" ];
+
+  meta = with lib; {
+    description = "Common loaders for MIR datasets";
+    homepage = "https://pypi.org/project/mirdata/";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ carlthome ];
+  };
+}
