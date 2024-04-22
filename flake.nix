ac8f1105214e26dc92e0d4110ec5189d09b035f9
Carl Thomé <carlthome@gmail.com>
2024-04-23 01:22:04 +0200

    Use nix-github-actions (#9)
    * Use nix-github-actions
    
    * Use packages
    
    * Don't fail fast
    
    * Write out argument
    
    * Use reusable workflow per build
    
    * Limit concurrency
    
    * Rename job to nix-matrix
    
    * Inline cachix cache
    
    * Limit concurrency

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
   description = "Nix packages for reproducible MIR research";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-github-actions = { url = "github:nix-community/nix-github-actions"; inputs.nixpkgs.follows = "nixpkgs"; };
+  };
 
-  outputs = { self, nixpkgs }@inputs:
+  outputs = { self, nixpkgs, nix-github-actions }@inputs:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -14,5 +17,8 @@
       devShells = forAllSystems (system: { default = import ./shell.nix { pkgs = nixpkgs.legacyPackages.${system}; }; });
       overlays.default = final: prev: (import ./pkgs/development/python-modules { pkgs = final; });
       templates.default = { path = ./examples/flake; description = "A basic Python environment with mirpkgs included as a nixpkgs overlay"; };
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = nixpkgs.lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ] self.packages;
+      };
     };
 }
