f3ac9a699b491f4d2ce54fc9763a8f1db833bfd1
Carl Thomé <carlthome@gmail.com>
2025-06-09 22:36:57 +0200

    Remove pytest-runner

--- pkgs/development/python-modules/x-transformers/default.nix
+++ pkgs/development/python-modules/x-transformers/default.nix
@@ -24,8 +24,6 @@ python3.pkgs.buildPythonApplication rec {
     torch
   ];
 
-  checkInputs = with python3.pkgs; [ pytest-runner ];
-
   pythonImportsCheck = [ "x_transformers" ];
 
   meta = with lib; {
