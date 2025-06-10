1f4f09f2c5a9b3f3904609313860bf3a8c779b11
Carl Thomé <carlthome@gmail.com>
2025-06-10 22:22:17 +0200

    Only expose packages

--- pkgs/development/python-modules/default.nix
+++ pkgs/development/python-modules/default.nix
@@ -3,6 +3,7 @@ let
   subdirectories = builtins.attrNames (
     pkgs.lib.filterAttrs (name: type: type == "directory") (builtins.readDir ./.)
   );
+
   mkScope =
     self:
     builtins.listToAttrs (
@@ -11,5 +12,14 @@ let
         value = self.callPackage (./. + "/${dir}") { };
       }) subdirectories
     );
+
+  scope = pkgs.lib.makeScope pkgs.newScope mkScope;
+
+  packages = builtins.listToAttrs (
+    map (dir: {
+      name = dir;
+      value = scope.${dir};
+    }) subdirectories
+  );
 in
-pkgs.lib.makeScope pkgs.newScope mkScope
+packages
