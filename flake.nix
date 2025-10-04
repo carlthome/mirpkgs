b5038f25e51ab2852f5b72eb2ffd1e5f6f986136
Carl Thomé <carlthome@gmail.com>
2025-10-04 02:41:04 +0200

    Use nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
       packages = forAllSystems (system: import ./default.nix (inputs // { inherit system; }));
       devShells = forAllSystems (system: {
         default = import ./shell.nix { pkgs = nixpkgs.legacyPackages.${system}; };
